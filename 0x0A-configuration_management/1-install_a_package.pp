6bdbc2c786d3878ef6385903bbd48624f5b110b9
Ayo Idoko <ayo.idoko007@gmail.com>
2023-12-27 08:19:24 +0200

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
 # install flask from pip3
+
+package { 'werkzeug' :
+    ensure   => '2.2.2',
+    provider => 'pip3'
+    }
+
 package { 'flask' :
     ensure   => '2.1.0',
     provider => 'pip3',
