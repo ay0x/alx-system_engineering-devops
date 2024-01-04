bacb050bb2c47874cf63a5d791663b2d361399d1
Ayo Idoko <ayo.idoko007@gmail.com>
2024-01-04 20:14:42 +0200

    Updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,31 @@
 # Install Nginx web server
-exec { 'server configuration':
-  provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; ufw allow 'Nginx HTTP'; echo 'Hello World!' > /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a \\\n\\tlocation /redirect_me {\\n\\t\\trewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\\n\\t}" /etc/nginx/sites-available/default; sudo service nginx start'
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => '/usr/bin:/usr/local/bin:/bin',
+}
+
+exec { 'apt-install-nginx':
+  command => '/usr/bin/apt-get -y install nginx',
+  path    => '/usr/bin:/usr/local/bin:/bin',
+}
+
+exec { 'ufw-allow-nginx-http':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"',
+  path    => '/usr/bin:/usr/local/bin:/bin',
+}
+
+exec { 'create-index-file':
+  command => '/bin/echo "Hello World!" > /var/www/html/index.nginx-debian.html',
+  path    => '/usr/bin:/usr/local/bin:/bin',
+}
+
+exec { 'configure-nginx':
+  command => '/bin/sed -i "/server_name _;/a \\n\\tlocation /redirect_me {\\n\\t\\trewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\\n\\t}" /etc/nginx/sites-available/default',
+  path    => '/usr/bin:/usr/local/bin:/bin',
+}
+
+exec { 'start-nginx-service':
+  command => '/usr/sbin/service nginx start',
+  path    => '/usr/bin:/usr/local/bin:/bin',
 }
