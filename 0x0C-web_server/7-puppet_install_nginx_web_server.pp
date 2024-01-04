51545ab1cb7c95b41f43eca06ac41b90feb1f027
Ayo Idoko <ayo.idoko007@gmail.com>
2024-01-04 05:27:23 +0200

    Updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
-# Install Nginx web server (w/ Puppet)
+# Install Nginx web server
 exec { 'server configuration':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; ufw allow 'Nginx HTTP'; echo 'Hello World!' > /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a \\\n\\tlocation /redirect_me {\\n\\t\\trewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\\n\\t}" /etc/nginx/sites-available/default; sudo service nginx start'
 }
