32fe5043fd9165fdbb3d4723133699e44302fc3a
ay0x <85720179+ay0x@users.noreply.github.com>
2024-02-14 11:45:15 +0200

    Fixed pycodestyle error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers"""
     URL = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7),\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
     r = requests.get(URL, headers=headers)
     if r.status_code == 200:
         r = r.json()
