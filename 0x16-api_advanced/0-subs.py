763a45196874ef922e8dcf638d36cb818e076c39
ay0x <85720179+ay0x@users.noreply.github.com>
2024-02-14 12:15:10 +0200

    Disable redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     URL = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7),\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
-    r = requests.get(URL, headers=headers)
+    r = requests.get(URL, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         r = r.json()
         return r['data']['subscribers']
